8209eba47786412324080cca17a997b9d75c3935
Diego97-webdev <shizusenpai97@gmail.com>
2020-07-24 14:46:03 -0300

    comprimidas las imagenes, comentado y organizado el codigo, agregado el menu desplegable

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
-$(".contenedor-carrete").slick({
-  dots: false,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  speed: 500,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  responsive: [
-    {
-      breakpoint: 580,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+$(document).ready(() => {
+  //CARRETE PROYECTOS PRESENCIALES / ONLINE
+  $(".contenedor-carrete").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-    {
-      breakpoint: 769,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
       },
-    },
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
-});
-$(".slick-vertical").slick({
-  dots: false,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  speed: 10000,
-  autoplay: true,
-  autoplaySpeed: 1,
-  vertical: true,
+    ],
+  });
+
+  //CARRETE TESTIMONIALES
+  $(".slick-vertical").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 10000,
+    autoplay: true,
+    autoplaySpeed: 1,
+    vertical: true,
+  });
+
+  //EVENT LISTENERS MENU DESPLEGABLE
+  $(".btn-cerrar").click(() => {
+    $(".menu-desplegable").css({
+      right: "-100%",
+    });
+  });
+
+  $(".hamburguesa").click(() => {
+    $(".menu-desplegable").css({
+      right: "0px",
+    });
+  });
+
+  $(".categorias").click(() => {
+    $(".menu-desplegable").css({
+      right: "-100%",
+    });
+  });
 });
